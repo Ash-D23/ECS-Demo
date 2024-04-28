a1106771a8eefbd28b3f18e33a00af7573891354
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2024-04-28 11:46:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 // Define a route
 app.get('/', (req, res) => {
-res.send('Congratulations HHLD Folks - we are in ECS Demo!');
+res.send('Congratulations HHLD Folks - we are in ECS Demo Github!');
 });
 
 app.get('/health', (req, res) => {
